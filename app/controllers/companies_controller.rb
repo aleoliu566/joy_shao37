a6f31b2c89e43111b9cf9f0fd09875737bed2574
leo <aleoliu566@gmail.com>
2017-11-07 00:50:58 +0800

    raw sql version 2

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
   def home
     @jobs = Job.all
-    @companies = Company.get_all_company
+    @companies = Company.get_all_company_v2
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -3,9 +3,12 @@ class Company < ApplicationRecord
   has_many :users
   has_many :articles
 
-  def self.get_all_company
-    sql = "SELECT * FROM companies"
-    all_companies = self.find_by_sql(sql)  # 最後一行是回傳值
+  def self.get_all_company_v2
+    query = <<-SQL
+    SELECT * FROM companies
+    SQL
+
+    all_companies = self.find_by_sql(query) # 最後一行是回傳值
   end
 
 end
