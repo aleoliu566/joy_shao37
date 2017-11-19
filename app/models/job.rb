dbbc6426fa24c20970b33bd2a537b8f66145f1e6
beibei0221 <ya0981012155@gmail.com>
2017-11-19 21:50:25 +0800

    修改企業管理之職缺管理內容

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,14 +4,14 @@ class Job < ApplicationRecord
   has_many :resume_jobships
   has_many :resumes, :through => :resume_jobships 
   #資料驗證
-  validates_presence_of :name, :published_on, :content, :hour_salary_ceiling, :hour_salary_floor, :year_salary_floor, :year_salary_ceiling
+  validates_presence_of :name, :published_on, :content, :salary
 
     #CREATE JOB
-    def self.hr_create_job(cid,n,p,c,hc,hf,yc,yf)
+    def self.hr_create_job(cid,n,p,c,s)
       t = DateTime.now
       query = <<-SQL
-      INSERT INTO jobs(company_id,name,published_on,content,hour_salary_ceiling,hour_salary_floor,year_salary_ceiling,year_salary_floor,created_at,updated_at)
-      VALUES ("#{cid}","#{n}","#{p}","#{c}","#{hc}","#{hf}","#{yc}","#{yf}","#{t}" ,"#{t}")
+      INSERT INTO jobs(company_id,name,published_on,content,salary,created_at,updated_at)
+      VALUES ("#{cid}","#{n}","#{p}","#{c}","#{s}","#{t}" ,"#{t}")
       SQL
       self.find_by_sql(query)
     end
@@ -41,11 +41,11 @@ class Job < ApplicationRecord
     end
 
     #UPDATE
-    def self.hr_update_job(j,n,p,c,hc,hf,yc,yf)
+    def self.hr_update_job(j,n,p,c,s)
       t = DateTime.now
       query = <<-SQL
       UPDATE jobs 
-      SET name = "#{n}", published_on = "#{p}", content = "#{c}", hour_salary_ceiling = "#{hc}", hour_salary_floor = "#{hf}", updated_at = "#{t}", year_salary_ceiling  = "#{yc}", year_salary_floor = "#{yf}"
+      SET name = "#{n}", published_on = "#{p}", content = "#{c}", salary = "#{s}", updated_at = "#{t}"
       WHERE id = "#{j}"
       SQL
       self.find_by_sql(query)
