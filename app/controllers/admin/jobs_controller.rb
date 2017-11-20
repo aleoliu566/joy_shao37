6ce31b56e21d162ab3a8f64cc7eccad01e205f0e
cindylai1010 <cindywhale1010@gmail.com>
2017-11-20 20:49:17 +0800

    change job management
    更改職缺管理

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -3,7 +3,10 @@ class Admin::JobsController < ApplicationController
   layout 'admin'
 
   def index
-    @jobs = Job.all
+    # @jobs = Job.all
+    @jobInfos = Job.getJobInfos
+
+    @jobResumes = Job.getJobResumes
 
   end
 end
