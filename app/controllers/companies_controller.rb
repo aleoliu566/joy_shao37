9b6c47db3a42e28b15575c42929cd714373223c3
beibei0221 <ya0981012155@gmail.com>
2017-11-22 20:18:27 +0800

    Update index style

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,7 +5,7 @@ class CompaniesController < ApplicationController
   	#更改呼叫 Job Model 中的 get_all_job 方法 （和開放/關閉職缺有關）
     @jobs = Job.get_all_job
     @companies = Company.all
-    @articles = Article.order(view_count: :desc)
+    @articles = Article.order(view_count: :desc).limit(2)
   end
 
   def index
