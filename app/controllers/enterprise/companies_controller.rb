d4d655f683210d9784b2daf4cfef5142a60c54ad
leo <aleoliu566@gmail.com>
2017-11-22 13:46:16 +0800

    minor fix of enterprise index page

--- app/controllers/enterprise/companies_controller.rb
+++ app/controllers/enterprise/companies_controller.rb
@@ -57,7 +57,7 @@ class Enterprise::CompaniesController < ApplicationController
   end
 
   def company_params
-    params.require(:company).permit(:name, :phone, :email, :address, :about, :user_ids,:logo)
+    params.require(:company).permit(:name, :phone, :email, :address, :about, :user_ids ,:scale ,:logo)
   end
   
 end
