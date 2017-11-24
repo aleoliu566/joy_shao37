bfae93ab4d6e5b82aee597b39e45fe8e203a8415
joyshao37 <102306001@nccu.edu.tw>
2017-11-25 00:50:42 +0800

    標籤使用次數->當公司被停權時，工作會消失，標籤也會減少

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -18,10 +18,13 @@ class Tag < ApplicationRecord
   def self.get_all_tags
   	 query = <<-SQL
       SELECT tags.*, COUNT(tag_jobships.tag_id) AS count
-      FROM tags
-      LEFT JOIN tag_jobships
-      ON tags.id = tag_jobships.tag_id
+      FROM tag_jobships
+      RIGHT JOIN tags ON tags.id = tag_jobships.tag_id
+      LEFT JOIN jobs ON jobs.id = tag_jobships.job_id
+      LEFT JOIN companies ON companies.id = jobs.company_id 
+      WHERE account_status IS NULL OR account_status = "open"
       GROUP BY tags.id
+
   	 SQL
   	 self.find_by_sql(query)
   end	
